548cfa0528c52a3778aa5965f72124a6699d1d5c
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-05-15 21:39:07 +0300

    chore: sürüm yükseltildi.

--- src/utils/releaseNotes.ts
+++ src/utils/releaseNotes.ts
@@ -2,6 +2,6 @@
 // Her güncellemede sadece buradaki 'notes' kısmını ve gerekiyorsa 'version' kısmını değiştirmeniz yeterlidir.
 
 export const APP_RELEASE_NOTES = {
-  version: "1.0.1", // İsteğe bağlı, takip amaçlı
-  notes: "1) Aynı yörüngede aynı matematik işlemlerinin denk gelme sorunu çözüldü.\n2) İlk açılışta güncelleme notlarını gösteren sistem eklendi.\n3) Genel performans ve yörünge yerleşimleri iyileştirildi."
+  version: "1.0.8", // İsteğe bağlı, takip amaçlı
+  notes: "1) Aynı yörüngede aynı matematik işlemlerinin denk gelmesi sorunu çözüldü.\n2) Güncellemelerden sonraki ilk açılışta güncelleme notlarını gösteren sistem eklendi.\n3) Genel performans ve yörünge yerleşimleri iyileştirildi."
 };
